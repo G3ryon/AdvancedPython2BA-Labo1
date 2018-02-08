dd7320c0538a0271ca5b7d93f277d50b1ed2f839
G3ryon <dtheo98@gmail.com>
2018-02-08 11:59:31 +0100

    Add functions utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
-
+import sqrt from math 
 def fact(n):
     """Computes the factorial of a natural number.
     
@@ -11,6 +11,13 @@ def fact(n):
     Throws: ValueError if n < 0
     """
 
+    rep=1
+    i=1
+    while i>n:
+        rep *= n
+        i += 1
+    return rep
+
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
     
@@ -18,7 +25,17 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+
+    root=()
+    delta = (b**2) - (4*a*c)
+    if delta == 0:
+        root=((-b)/(2*a))
+        return (root,)
+    elif delta > 0:
+        root=((-b + sqrt(delta))/(2*a),(-b - sqrt(delta))/(2*a))
+        return root
+    else:
+        return root
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -29,9 +46,11 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+    rep = (upper - lower)*(eval(function, {'x': lower}) + (eval(function, {'x': upper})))/2 
+    return rep
 
 if __name__ == '__main__':
     print(fact(5))
     print(roots(1, 0, 1))
     print(integrate('x ** 2 - 1', -1, 1))
+    
